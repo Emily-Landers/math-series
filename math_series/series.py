51148f72f17020bd21fa3c84876466fc6bbf8b82
Emily Landers <emily.j.landers@gmail.com>
2022-05-01 20:59:52 -0400

    finished

--- math_series/series.py
+++ math_series/series.py
@@ -1,18 +1,26 @@
 def fibonacci(n):
+    '''Takes in a number n, return the nth value in the fibonacci series'''
+
     if n <= 1:
         return n 
     else:
         return (fibonacci(n - 1) + fibonacci(n - 2))
     
 def lucas(n):
+    '''Takes in a number n, that returns the nth value in the lucas numbers '''
+
     if (n == 0):
-            return 2;
+        return 2
+        
     if (n == 1):
-            return 1;
+        return 1
+        
     else:
         return lucas(n - 1) + lucas(n - 2);
 
 def sum_series(n, first = 0, second = 1):
+    '''Takes in a number n, if n is zero returns zero, if its one, returns one, and then will determine the first two values for the series to be produced.'''
+
     if n == 0:
         return first
     if n == 1:
